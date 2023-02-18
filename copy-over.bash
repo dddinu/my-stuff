4c76af433690978049a92b2943ac12df44aa5582
Daniel Dinu <ddd73@drexel.edu>
2023-02-18 12:04:38 -0500

    daily commit Sat Feb 18 12:04:38 PM EST 2023

--- copy-over.bash
+++ copy-over.bash
@@ -11,9 +11,13 @@ done < "${D_FILE}"
 # copy over scripts
 S_DIR="${HOME}/my-stuff/scripts/"
 S_FILE="${S_DIR}scripts.txt"
+if [[ ! -d "${HOME}/scripts" ]]
+then
+    mkdir "${HOME}/scripts"
+fi
 while read f
 do
-    cp -v "${S_DIR}${f}" "${HOME}/."
+    cp -v "${S_DIR}${f}" "${HOME}/scripts/."
 done < "${S_FILE}"
 
 exit 0
