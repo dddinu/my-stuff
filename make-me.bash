26c2505b110e800629f8ef8339a3c870faa09ee9
Daniel Dinu <ddd73@drexel.edu>
2023-02-19 10:30:02 -0500

    daily commit Sun Feb 19 10:30:02 AM EST 2023

--- make-me.bash
+++ make-me.bash
@@ -3,10 +3,18 @@
 MAIN_DIR="${HOME}/my-stuff/"
 
 # copy over scripts and dotfiles
+echo "copying scripts and dotfiles"
 "${MAIN_DIR}copy-over.bash"
 
 # copy over emacs packages
+echo "copying emacs packages"
 cp -r "${MAIN_DIR}.emacs.d/" "${HOME}/."
 
 # modify dotfiles appropriately
+
+# .bashrc modifications
+# remove aliases
+# change prompt
+# remove tigress settings
+# remove OD-LLVM settings
 
